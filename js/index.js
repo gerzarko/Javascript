11cb60cd26c966d50108d661644f88d24f6491c5
German Zarkovich <germanzarkovich@germans-mbp.myfiosgateway.com>
2022-06-13 17:31:39 -0400

    agrego funciones

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 
+function parse(elementoACambiar) {
+	elementoACambiar = parseInt(elementoACambiar);
+  	return elementoACambiar;
+}
+function calcularTotal(cantidad,precio) {
+	for (let i = 1; i <= cantidad; i++) {
+		let total= 0;	
+		total = cantidad * precio;
+		return total;
+}}
 
-alert("Bienvenido a la vinoteca de Cleopatra");
-
+alert("Bienvenido a la vinoteca Cleopatra");
 let respuesta;
 respuesta = prompt("Que andas buscando?\n Selecciona del siguiente catalogo indicando el numero de lo que queres encargar\n 1.Vino\n 2.Vodka\n 3:Cerveza\n 4:Licores");
 respuesta = parseInt(respuesta);
@@ -21,25 +30,23 @@ if (respuesta == 1  ) {
 	}
 
 	while (cantidadElegida > 12  && cantidadElegida < 1 )  {
-		cantidadElegida = prompt("Ahora selecciona la cantidad, recorda que para encargos mayores a 12tenes que llamarnos al 0112435412");
-	  cantidadElegida = parseInt(cantidadElegida);
+		cantidadElegida = prompt("Ahora selecciona la cantidad, recorda que para encargos mayores a 12 botellas tenes que llamarnos al 0112435412");
+	  	parse(cantidadElegida);
 
-  	
 	}
 
   	if (vinoElegido == 1 ) {
-  		for (let i= 1; i <= cantidadElegida; i++) {
-			totalPago = 500*i;
-	
-  	}}
-	if (vinoElegido == 2 ) {
-  		for (let i= 1; i <= cantidadElegida; i++) {
-			totalPago = 300*i;
-	
-  	}
-		 		
+  	 	totalPago = calcularTotal(cantidadElegida,500)	;
+		alert(totalPago);
 	}
-	alert(totalPago);
-  	}
 
+	if (vinoElegido == 2 ) {
+	  	 totalPago = calcularTotal(cantidadElegida, 300);
+		alert(totalPago);
+
+	}
+		 		
+  	
+	
+}
 
